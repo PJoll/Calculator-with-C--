6ec9cea57e2c89089d8459d028a660f766f0d8a7
Peter Jolley <peter@marango.com.au>
2023-06-01 16:27:44 +0930

    number handler and update number complete button inludes onClick with props

--- src/Calculator.js
+++ src/Calculator.js
@@ -2,10 +2,27 @@ import React from "react";
 import CalculatorPanel from "./CalculatorPanel";       
 
 class Calculator extends React.Component{
-    operationHandler(operation){
-
+    constructor(props){
+        super(props);
+        this.state = {
+            result: "",
+            numbers: ["",""],
+            numberIdx: 0,
+        };
     }
-    numberHandler(number){}
+    operationHandler(operation){}
+    numberHandler(number){
+        const newNumber = this.state.numbers[this.state.numberIdx] + number;
+        this.updateNumber(newNumber);
+    }
+    updateNumber(newNumber){
+        var newNumbers = this.state.numbers;
+        newNumbers[this.state.numberIdx] = newNumber;
+        this.setState({
+            result: newNumbers[this.state.numberIdx],
+            numbers: newNumbers,
+        })
+    }
     dotHandler(){}
     clearHandler(){}
     equalHandler(){}
@@ -13,6 +30,7 @@ class Calculator extends React.Component{
         return(
             <div className="calculator-grid-container">
                 <CalculatorPanel
+                result={this.state.result}
                 numberClicked={this.numberHandler.bind(this)}
                 operationClicked={this.operationHandler.bind(this)}
                 dotClicked={this.dotHandler.bind(this)}
--- src/CalculatorButton.js
+++ src/CalculatorButton.js
@@ -2,8 +2,12 @@ import React  from 'react';
 
 function CalculatorButton(props) {
   return (
-    <input type='button'
-    value={props.caption}/>
+    <input 
+    type='button'
+    value={props.caption}
+    className={props.caption === "=" ? "calculator-button-equal" : null}
+    onClick={props.onClick}
+    />
   );
 }
 
