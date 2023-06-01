9030943bb49ec2814ae97e7a300acd3a509c3bf9
Peter Jolley <peter@marango.com.au>
2023-06-01 16:08:01 +0930

    simple adaption preparing for onClick handerls for each operation

---
+++ src/Calculator.js
@@ -0,0 +1,28 @@
+import React from "react";
+import CalculatorPanel from "./CalculatorPanel";       
+
+class Calculator extends React.Component{
+    operationHandler(operation){
+
+    }
+    numberHandler(number){}
+    dotHandler(){}
+    clearHandler(){}
+    equalHandler(){}
+    render(){
+        return(
+            <div className="calculator-grid-container">
+                <CalculatorPanel
+                numberClicked={this.numberHandler.bind(this)}
+                operationClicked={this.operationHandler.bind(this)}
+                dotClicked={this.dotHandler.bind(this)}
+                clearClicked={this.clearHandler.bind(this)}
+                equalClicked={this.equalHandler.bind(this)}
+                />
+            </div>
+
+        );
+    }
+}
+
+export default Calculator;
